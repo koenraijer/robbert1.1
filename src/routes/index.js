098d25d2916d1055e71bab69201c768dc7d3d07d
Koen Raijer <koenrayer@gmail.com>
2022-06-28 13:16:51 +0200

    photos larger

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,13 +1,14 @@
 import {client} from '$lib/js/graphql-client'
 import {projectsQuery} from '$lib/js/graphql-queries'
+import {config} from '$lib/js/stores'
 
 export async function get() {
         let sm = 640;
         let md = 768;
         let lg = 1024;
         let xl = 1280;
-        let xxl = 1536;
-
+        let xxl = 2560; // 1536 oud
+        
         const variables = {sm, md, lg, xl, xxl}
 
         const {projects} = await client.request(projectsQuery, variables) 
